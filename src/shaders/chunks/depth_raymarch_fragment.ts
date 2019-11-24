05a3e0828505389f5fae4e19bec3bc1eb8a7039f
aadebdeb <aa.debdeb@gmail.com>
2019-11-24 21:28:53 +0900

    change order of program

--- src/shaders/chunks/depth_raymarch_fragment.ts
+++ src/shaders/chunks/depth_raymarch_fragment.ts
@@ -3,13 +3,12 @@ Ray ray = convertRayFromWorldToObject(createRay());
 
 vec2 range = getRaymarchRange(ray);
 float t;
-vec3 position; // for fog
-if (raymarch(ray, range.x, range.y, t)) {
-  position = ray.origin + t * ray.direction;
-  vec4 clipPos = mvpMatrix * vec4(position, 1.0);
-  gl_FragDepthEXT = (clipPos.z / clipPos.w) * 0.5 + 0.5;
-  gl_FragColor = vec4(vec3(1.0 - gl_FragDepthEXT), opacity);
-} else {
+if (!raymarch(ray, range.x, range.y, t)) {
   discard;
+  return;
 }
+vec3 position = ray.origin + t * ray.direction;
+vec4 clipPos = mvpMatrix * vec4(position, 1.0);
+gl_FragDepthEXT = (clipPos.z / clipPos.w) * 0.5 + 0.5;
+gl_FragColor = vec4(vec3(1.0 - gl_FragDepthEXT), opacity);
 `;
--- src/shaders/chunks/raymarch_fragment.ts
+++ src/shaders/chunks/raymarch_fragment.ts
@@ -3,14 +3,13 @@ Ray ray = convertRayFromWorldToObject(createRay());
 
 vec2 range = getRaymarchRange(ray);
 float t;
-vec3 position; // for fog
-if (raymarch(ray, range.x, range.y, t)) {
-  position = ray.origin + t * ray.direction;
-  vec3 normal = t == range.x ? normalize(vRaymarchNormal) : normalMatrix * getNormal(position);
-  gl_FragColor = vec4(getColor(position, normal), opacity);
-  vec4 clipPos = mvpMatrix * vec4(position, 1.0);
-  gl_FragDepthEXT = (clipPos.z / clipPos.w) * 0.5 + 0.5;
-} else {
+if (!raymarch(ray, range.x, range.y, t)) {
   discard;
+  return;
 }
+vec3 position = ray.origin + t * ray.direction;
+vec3 normal = t == range.x ? normalize(vRaymarchNormal) : normalMatrix * getNormal(position);
+gl_FragColor = vec4(getColor(position, normal), opacity);
+vec4 clipPos = mvpMatrix * vec4(position, 1.0);
+gl_FragDepthEXT = (clipPos.z / clipPos.w) * 0.5 + 0.5;
 `;
